65b687822a0c48ff176b0e5ec1c907e1ea564bd0
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2025-05-05 19:26:13 +0100

    files restructured and routing fixed and package json updated

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,9 +1,10 @@
+import React from "react";
 import { View, Text } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Chat Screen</Text>
+      <Text>Home Screen</Text>
     </View>
   );
 }
