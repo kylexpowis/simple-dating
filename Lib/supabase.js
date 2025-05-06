847daa68c9e1c7e51afe2d60daec15b21b2c11db
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2025-05-06 21:17:12 +0100

    added supabase client info

---
+++ Lib/supabase.js
@@ -0,0 +1,5 @@
+// lib/supabase.js
+import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_ANON_KEY, SUPABASE_URL } from "@env";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
