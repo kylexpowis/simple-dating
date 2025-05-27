f6c4bf14bf80b2a8052b333a529067d2d3d9a79e
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2025-05-27 18:15:56 +0100

    add singlechatscreen to the stack navigator

--- src/navigation/HomeStackNavigator.jsx
+++ src/navigation/HomeStackNavigator.jsx
@@ -10,6 +10,13 @@ export default function HomeStackNavigator() {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="HomeList" component={HomeScreen} />
       <Stack.Screen name="OtherUserProfile" component={OtherUserProfile} />
+      <Stack.Screen
+        name="SingleChat"
+        component={SingleChatScreen}
+        options={({ route }) => ({
+          title: route.params.otherUser.firstName,
+        })}
+      />
     </Stack.Navigator>
   );
 }
